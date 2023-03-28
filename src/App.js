2ffc5ee8474a96ae3d97cd825b6ff6676f5d132a
yrajeshtechsierra <rajesh.yadav@techsierra.in>
2023-03-28 11:40:49 +0530

    created an drag and drop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import userdata from "./temData.json";
+import { useState } from "react";
+
+export default function App() {
+  const [users, setUsers] = useState(userdata.data);
+
+  const handleDragEnd = (e) => {
+    if (!e.destination) return;
+    let tempData = Array.from(users);
+    let [source_data] = tempData.splice(e.source.index, 1);
+    tempData.splice(e.destination.index, 0, source_data);
+    setUsers(tempData);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mt-4">
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <table className="table borderd">
+          <thead>
+            <tr>
+              <th />
+              <th>Username</th>
+              <th>Age</th>
+              <th>Gender</th>
+            </tr>
+          </thead>
+          <Droppable droppableId="droppable-1">
+            {(provider) => (
+              <tbody
+                className="text-capitalize"
+                ref={provider.innerRef}
+                {...provider.droppableProps}
+              >
+                {users?.map((user, index) => (
+                  <Draggable
+                    key={user.name}
+                    draggableId={user.name}
+                    index={index}
+                  >
+                    {(provider) => (
+                      <tr {...provider.draggableProps} ref={provider.innerRef}>
+                        <td {...provider.dragHandleProps}> = </td>
+                        <td>{user.name}</td>
+                        <td>{user.age}</td>
+                        <td>{user.gender}</td>
+                      </tr>
+                    )}
+                  </Draggable>
+                ))}
+                {provider.placeholder}
+              </tbody>
+            )}
+          </Droppable>
+        </table>
+      </DragDropContext>
     </div>
   );
 }
-
-export default App;
